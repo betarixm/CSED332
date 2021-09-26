0d8846ecf1b6b410bb3924d92b7e35dff7aca9dd
beta <mzg00@postech.ac.kr>
2021-09-27 02:50:00 +0900

    💯 Multiple tests for Exp

--- problem2/src/test/java/edu/postech/csed332/homework2/ExpTest.java
+++ problem2/src/test/java/edu/postech/csed332/homework2/ExpTest.java
@@ -2,6 +2,9 @@ package edu.postech.csed332.homework2;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ExpTest {
@@ -19,10 +22,153 @@ public class ExpTest {
         });
     }
 
-    /*
-     * TODO: add  test methods to achieve at least 80% branch coverage of the classes:
-     *  Conjunction, Constant, Disjunction, Negation, Variable.
-     * Each test method should have appropriate JUnit assertions to test a single behavior.
-     * You should not add arbitrary code to test methods to just increase coverage.
-     */
+    @Test
+    void testConjunctionSimpleEvaluateExps() {
+        assertEquals(ExpParser.parse("true && true").evaluate(Map.of()), true);
+        assertEquals(ExpParser.parse("true && false").evaluate(Map.of()), false);
+        assertEquals(ExpParser.parse("false && false").evaluate(Map.of()), false);
+    }
+
+    @Test
+    void testConstantSimpleEvaluateExps() {
+        assertEquals(ExpParser.parse("true").evaluate(Map.of()), true);
+        assertEquals(ExpParser.parse("false").evaluate(Map.of()), false);
+    }
+
+    @Test
+    void testDisjunctionSimpleEvaluateExps() {
+        assertEquals(ExpParser.parse("true || true").evaluate(Map.of()), true);
+        assertEquals(ExpParser.parse("true || false").evaluate(Map.of()), true);
+        assertEquals(ExpParser.parse("false || false").evaluate(Map.of()), false);
+    }
+
+    @Test
+    void testNegationSimpleEvaluateExps() {
+        assertEquals(ExpParser.parse("! true").evaluate(Map.of()), false);
+        assertEquals(ExpParser.parse("! false").evaluate(Map.of()), true);
+    }
+
+    @Test
+    void testVariableSimpleEvaluateExps() {
+        assertEquals(ExpParser.parse("p1").evaluate(Map.of(1, true)), true);
+        assertEquals(ExpParser.parse("p1").evaluate(Map.of(1, false)), false);
+
+        assertEquals(ExpParser.parse("p1 && p2").evaluate(Map.of(1, true, 2, true)), true);
+        assertEquals(ExpParser.parse("p1 && p2").evaluate(Map.of(1, true, 2, false)), false);
+        assertEquals(ExpParser.parse("p1 && p2").evaluate(Map.of(1, false, 2, false)), false);
+
+        assertEquals(ExpParser.parse("p1 || p2").evaluate(Map.of(1, true, 2, true)), true);
+        assertEquals(ExpParser.parse("p1 || p2").evaluate(Map.of(1, true, 2, false)), true);
+        assertEquals(ExpParser.parse("p1 || p2").evaluate(Map.of(1, false, 2, false)), false);
+    }
+
+    @Test
+    void testIdentityLaw() {
+        assertEquals(ExpParser.parse("p1 && true").simplify().toString(), "(p1)");
+        assertEquals(ExpParser.parse("p1 || false").simplify().toString(), "(p1)");
+    }
+
+    @Test
+    void testIdempotentLaw() {
+        assertEquals(ExpParser.parse("p1 && p1").simplify().toString(), "(p1)");
+        assertEquals(ExpParser.parse("p1 || p1").simplify().toString(), "(p1)");
+    }
+
+    @Test
+    void testDominationLaw() {
+        assertEquals(ExpParser.parse("p1 && false").simplify().evaluate(Map.of()), false);
+        assertEquals(ExpParser.parse("p1 || true").simplify().evaluate(Map.of()), true);
+    }
+
+    @Test
+    void testNegationLaw() {
+        assertEquals(ExpParser.parse("p1 && ! p1").simplify().evaluate(Map.of()), false);
+        assertEquals(ExpParser.parse("p1 || ! p1").simplify().evaluate(Map.of()), true);
+    }
+
+    @Test
+    void testAbsorptionLaw() {
+        assertEquals(ExpParser.parse("p1 || ( p1 && p2 )").simplify().toString(), "(p1)");
+        assertEquals(ExpParser.parse("p1 && ( p1 || p2 )").simplify().toString(), "(p1)");
+    }
+
+    @Test
+    void testDeMorgansLaws() {
+        assertEquals(ExpParser.parse("!(p1 && p2)").simplify().toString(), "((! p1) || (! p2))");
+        assertEquals(ExpParser.parse("!(p1 || p2)").simplify().toString(), "((! p1) && (! p2))");
+    }
+
+    @Test
+    void testDoubleNegationLaw() {
+        assertEquals(ExpParser.parse("! ( ! p1 )").simplify().toString(), "(p1)");
+    }
+
+    @Test
+    void testDistributiveLaws() {
+        assertEquals(ExpParser.parse("p1 || ( p2 && p3 )").simplify().toString(), "((p1 || p2) && (p1 || p3))");
+        assertEquals(ExpParser.parse("p1 && ( p2 || p3 )").simplify().toString(), "((p1 && p2) || (p1 && p3))");
+    }
+
+    @Test
+    void testConjunctionEquals() {
+        Conjunction n1 = new Conjunction(new Constant(true));
+        Conjunction n2 = new Conjunction(new Constant(false));
+
+        assertEquals(n1, n1);
+        assertNotEquals(n1, n2);
+    }
+
+    @Test
+    void testDisjunctionEquals() {
+        Disjunction n1 = new Disjunction(new Constant(true));
+        Disjunction n2 = new Disjunction(new Constant(false));
+
+        assertEquals(n1, n1);
+        assertNotEquals(n1, n2);
+    }
+
+    @Test
+    void testNegationEquals() {
+        Negation n1 = new Negation(new Constant(true));
+        Negation n2 = new Negation(new Constant(false));
+
+        assertEquals(n1, n1);
+        assertNotEquals(n1, n2);
+    }
+
+    @Test
+    void testVariableEquals() {
+        Variable n1 = new Variable(1);
+        Variable n2 = new Variable(2);
+
+        assertEquals(n1, n1);
+        assertNotEquals(n1, n2);
+    }
+
+    @Test
+    void testConstantEquals() {
+        Constant n1 = new Constant(true);
+        Constant n2 = new Constant(false);
+
+        assertEquals(n1, n1);
+        assertNotEquals(n1, n2);
+        assertNotEquals(n1, null);
+        assertNotEquals(n1, new Variable(1));
+    }
+
+    @Test
+    void testVars() {
+        Exp exp = ExpParser.parse("p1 || p2 && ! p3 || true");
+        assertEquals(exp.vars(), Set.of(1, 2, 3));
+    }
+
+    @Test
+    void testIllegalVariable() {
+        assertThrows(IllegalArgumentException.class, () -> new Variable(-1));
+    }
+
+    @Test
+    void testGetIdentifier() {
+        assertEquals((new Variable(1)).getIdentifier(), 1);
+    }
 }
