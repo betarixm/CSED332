c3c02e52433a03a7c94647c1407a9525a56d914f
beta <mzg00@postech.ac.kr>
2021-09-27 02:47:20 +0900

    ﹠ Impl. Conjunction

--- problem2/src/main/java/edu/postech/csed332/homework2/Conjunction.java
+++ problem2/src/main/java/edu/postech/csed332/homework2/Conjunction.java
@@ -1,24 +1,19 @@
 package edu.postech.csed332.homework2;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * A Boolean expression whose top-level operator is && (and).
  */
-public class Conjunction implements Exp {
-    private final List<Exp> subexps;
-
+public class Conjunction extends BinaryExp {
     /**
      * Builds a conjunction of a given list of Boolean expressions
      *
      * @param exps
      */
     public Conjunction(Exp... exps) {
-        subexps = Arrays.asList(exps);
+        super(Arrays.asList(exps));
     }
 
     /**
@@ -31,21 +26,91 @@ public class Conjunction implements Exp {
     }
 
     @Override
-    public Set<Integer> vars() {
-        // TODO: implement this
-        return null;
-    }
-
-    @Override
     public Boolean evaluate(Map<Integer, Boolean> assignment) {
-        // TODO: implement this
-        return null;
+        return subexps.stream().allMatch(e -> e.evaluate(assignment));
     }
 
     @Override
     public Exp simplify() {
-        // TODO: implement this
-        return null;
+        Constant FALSE = new Constant(false);
+
+        // (0) Simplify subexps
+        List<Exp> exps = subexps.stream().map(Exp::simplify).toList();
+
+        // (1) Identity and idempotent laws
+        exps = exps.stream().filter(e -> {
+            return !((e instanceof Constant) && ((Constant) e).getValue());
+        }).toList();
+
+        exps = exps.stream().distinct().toList();
+
+        // (2) Domination and negation laws
+        if (exps.stream().anyMatch(e -> e.equals(FALSE))) {
+            return FALSE;
+        }
+
+        List<Exp> negationSubexps = exps.stream()
+                .filter(e -> e instanceof Negation)
+                .map(e -> ((Negation) e).getSubexp())
+                .toList();
+
+        if (exps.stream().anyMatch(negationSubexps::contains)) {
+            return FALSE;
+        }
+
+        // (4) Absorption laws
+        List<Disjunction> disjunctionSubexps = exps.stream()
+                .filter(e -> e instanceof Disjunction)
+                .map(e -> (Disjunction) e)
+                .toList();
+
+        List<Exp> finalExps = exps;
+        disjunctionSubexps = disjunctionSubexps.stream().filter(d -> {
+            for (Exp _d : d.getSubexps()) {
+                if (finalExps.contains(_d)) {
+                    return false;
+                }
+            }
+            return true;
+        }).toList();
+
+
+        List<Disjunction> finalDisjunctionSubexps = disjunctionSubexps;
+        exps = exps.stream()
+                .filter(e -> !(e instanceof Disjunction && !finalDisjunctionSubexps.contains(e)))
+                .toList();
+
+        if (exps.size() < 2) {
+            return new Conjunction(exps.get(0));
+        }
+
+        // (6) Distributive laws
+        List<Exp> tmpExps = new ArrayList<>();
+
+        for (int cursor = 0; cursor < exps.size() - 1; cursor++) {
+            if (exps.get(cursor + 1) instanceof Disjunction) {
+                Exp cur = exps.get(cursor);
+
+                tmpExps.add(new Disjunction(
+                        ((Disjunction) exps.get(cursor + 1)).getSubexps().stream()
+                                .map(e -> new Conjunction(cur, e))
+                                .toList().toArray(new Exp[0])
+                ));
+
+                cursor++;
+            } else {
+                tmpExps.add(exps.get(cursor));
+                if (cursor == exps.size() - 2) {
+                    tmpExps.add(exps.get(cursor + 1));
+                }
+            }
+        }
+
+        if (tmpExps.size() == 1) {
+            return tmpExps.get(0);
+        }
+
+        return new Conjunction(tmpExps.toArray(new Exp[0]));
     }
 
     @Override
