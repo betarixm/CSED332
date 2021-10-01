3c8bbb39928f54214a86160154d34dfa4e0f4c50
beta <mzg00@postech.ac.kr>
2021-10-01 21:51:51 +0900

    🌳 Impl. ParentPointerTree

--- src/main/java/edu/postech/csed332/homework3/ParentPointerTree.java
+++ src/main/java/edu/postech/csed332/homework3/ParentPointerTree.java
@@ -47,80 +47,124 @@ public class ParentPointerTree<N extends Comparable<N>> implements MutableTree<N
 
     @Override
     public @NotNull Optional<N> getRoot() {
-        // TODO: implement this
-        return Optional.empty();
+        return root == null ? Optional.empty() : Optional.of(root);
     }
 
     @Override
     public int getDepth(@NotNull N vertex) {
-        // TODO: implement this
-        return 0;
+        if (getRoot().isEmpty()) {
+            throw new IllegalStateException();
+        } else if (!nodeMap.containsKey(vertex)) {
+            throw new IllegalArgumentException();
+        }
+
+        return nodeMap.get(vertex).depth;
     }
 
     @Override
     public int getHeight() {
-        // TODO: implement this
-        return 0;
+        if (getRoot().isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        return nodeMap.values().stream().mapToInt(n -> n.depth).max().orElse(0);
     }
 
     @Override
     public boolean containsVertex(@NotNull N vertex) {
-        // TODO: implement this
-        return false;
+        return nodeMap.containsKey(vertex);
     }
 
     @Override
     public boolean addVertex(@NotNull N vertex) {
-        // TODO: implement this
-        return false;
+        if (getRoot().isEmpty()) {
+            root = vertex;
+            nodeMap.put(vertex, new Node<>(null, 0));
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean removeVertex(@NotNull N vertex) {
-        // TODO: implement this
-        return false;
+        if (containsVertex(vertex)) {
+            Set<N> target = new HashSet<>(findReachableVertices(vertex));
+            Iterator<N> iter = target.iterator();
+
+            while (iter.hasNext()) {
+                N v = iter.next();
+                if (v == root) {
+                    root = null;
+                }
+                nodeMap.remove(v);
+                iter.remove();
+            }
+
+            return true;
+
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean containsEdge(@NotNull N source, @NotNull N target) {
-        // TODO: implement this
-        return false;
+        return nodeMap.get(target) != null && Objects.equals(nodeMap.get(target).parent, source);
     }
 
     @Override
     public boolean addEdge(@NotNull N source, @NotNull N target) {
-        // TODO: implement this
-        return false;
+        if (nodeMap.containsKey(source) && !nodeMap.containsKey(target)) {
+            nodeMap.put(target, new Node<>(source, nodeMap.get(source).depth + 1));
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     @Override
     public boolean removeEdge(@NotNull N source, @NotNull N target) {
-        // TODO: implement this
-        return false;
+        if (containsEdge(source, target)) {
+            removeVertex(target);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public @NotNull Set<N> getSources(N target) {
-        // TODO: implement this
-        return Collections.emptySet();
+        N parent = nodeMap.get(target).parent;
+
+        if (parent == null) {
+            return Collections.emptySet();
+        } else {
+            return Set.of(parent);
+        }
     }
 
     @Override
     public @NotNull Set<N> getTargets(N source) {
-        // TODO: implement this
-        return Collections.emptySet();
+        return nodeMap.entrySet().stream()
+                .filter(e -> e.getValue().parent == source)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     @Override
     public @NotNull Set<N> getVertices() {
-        // TODO: implement this
-        return Collections.emptySet();
+        return Collections.unmodifiableSet(nodeMap.keySet());
     }
 
     @Override
     public @NotNull Set<Edge<N>> getEdges() {
-        // TODO: implement this
-        return Collections.emptySet();
+        return nodeMap.entrySet().stream()
+                .filter(e -> e.getValue().parent != null)
+                .map(e -> new Edge<>(e.getValue().parent, e.getKey()))
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     /**
@@ -129,8 +173,33 @@ public class ParentPointerTree<N extends Comparable<N>> implements MutableTree<N
      * @return true if the representation of this tree is valid
      */
     boolean checkInv() {
-        // TODO: implement this
-        return false;
+        Set<N> vertices = getVertices();
+        Set<Edge<N>> edges = getEdges();
+
+        for (Edge<N> e : edges) {
+            N s = e.getSource();
+            N t = e.getTarget();
+
+            if ((!vertices.contains(s)) || (!vertices.contains(t))) {
+                return false;
+            }
+
+            if (t == root) {
+                return false;
+            }
+
+            if (root != null && !findReachableVertices(root).equals(getVertices())) {
+                return false;
+            }
+        }
+
+        for(N n : vertices) {
+            if (n != root && getSources(n).size() != 1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
