99a8d9497026e0ad90dfbff031ea3d5c9f6f86c1
beta <mzg00@postech.ac.kr>
2021-10-01 20:54:14 +0900

    💯 Impl. AbstractMutableTreeTest

--- src/test/java/edu/postech/csed332/homework3/AbstractMutableTreeTest.java
+++ src/test/java/edu/postech/csed332/homework3/AbstractMutableTreeTest.java
@@ -44,6 +44,122 @@ public abstract class AbstractMutableTreeTest<V extends Comparable<V>, T extends
         Assertions.assertThrows(IllegalArgumentException.class, () -> tree.getDepth(v2));
     }
 
-    // TODO: write black-box test casess for each method of MutableTree with respect to
-    //  the specification, including the methods of Tree that MutableTree extends.
+    @Test
+    void testGetHeight() {
+        Assertions.assertTrue(tree.addVertex(v1));
+        Assertions.assertTrue(tree.addEdge(v1, v2));
+        Assertions.assertTrue(tree.addEdge(v2, v3));
+        Assertions.assertTrue(tree.addEdge(v3, v4));
+        Assertions.assertTrue(tree.addEdge(v1, v5));
+        Assertions.assertTrue(tree.addEdge(v5, v7));
+        Assertions.assertTrue(tree.addEdge(v5, v6));
+        Assertions.assertTrue(tree.addEdge(v7, v8));
+
+        Assertions.assertEquals(tree.getHeight(), 3);
+
+        Assertions.assertTrue(checkInv());
+    }
+
+    @Test
+    void testGetHeightNoRoot() {
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            tree.getHeight();
+        });
+
+        Assertions.assertTrue(checkInv());
+    }
+
+    @Test
+    void testContainsVertex() {
+        Assertions.assertTrue(tree.addVertex(v1));
+        Assertions.assertTrue(tree.addEdge(v1, v2));
+
+        Assertions.assertTrue(tree.containsVertex(v1));
+        Assertions.assertTrue(tree.containsVertex(v2));
+        Assertions.assertFalse(tree.containsVertex(v3));
+
+        Assertions.assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddVertexAlreadyAdded() {
+        Assertions.assertTrue(tree.addVertex(v1));
+        Assertions.assertFalse(tree.addVertex(v2));
+    }
+
+    @Test
+    void testRemoveVertex() {
+        Assertions.assertTrue(tree.addVertex(v1));
+        Assertions.assertTrue(tree.addEdge(v1, v2));
+        Assertions.assertTrue(tree.addEdge(v2, v3));
+        Assertions.assertTrue(tree.addEdge(v3, v4));
+        Assertions.assertTrue(tree.addEdge(v1, v5));
+        Assertions.assertTrue(tree.addEdge(v5, v7));
+        Assertions.assertTrue(tree.addEdge(v5, v6));
+        Assertions.assertTrue(tree.addEdge(v7, v8));
+
+        Assertions.assertTrue(tree.removeVertex(v5));
+        Assertions.assertFalse(tree.removeVertex(v5));
+
+        Assertions.assertTrue(tree.removeVertex(v4));
+        Assertions.assertFalse(tree.removeVertex(v4));
+
+        Assertions.assertEquals(tree.getVertices(), Set.of(v1, v2, v3));
+
+        for(V v : Set.of(v4, v5, v6, v7, v8)) {
+            Assertions.assertFalse(tree.containsVertex(v));
+        }
+
+        Assertions.assertEquals(tree.getHeight(), 2);
+
+        Assertions.assertTrue(checkInv());
+    }
+
+    @Test
+    void testRemoveVertexRoot() {
+        Assertions.assertTrue(tree.addVertex(v1));
+        Assertions.assertTrue(tree.removeVertex(v1));
+        Assertions.assertTrue(tree.getRoot().isEmpty());
+    }
+
+    @Test
+    void testRemoveEdge() {
+        Assertions.assertTrue(tree.addVertex(v1));
+        Assertions.assertTrue(tree.addEdge(v1, v2));
+        Assertions.assertTrue(tree.addEdge(v2, v3));
+        Assertions.assertTrue(tree.addEdge(v3, v4));
+        Assertions.assertTrue(tree.addEdge(v1, v5));
+        Assertions.assertTrue(tree.addEdge(v5, v7));
+        Assertions.assertTrue(tree.addEdge(v5, v6));
+        Assertions.assertTrue(tree.addEdge(v7, v8));
+
+        Assertions.assertTrue(tree.removeEdge(v1, v5));
+        Assertions.assertFalse(tree.removeEdge(v1, v5));
+
+        Assertions.assertTrue(tree.removeEdge(v3, v4));
+        Assertions.assertFalse(tree.removeEdge(v3, v4));
+
+        Assertions.assertFalse(tree.removeEdge(v4, v8));
+
+        Assertions.assertEquals(tree.getVertices(), Set.of(v1, v2, v3));
+
+        for(V v : Set.of(v4, v5, v6, v7, v8)) {
+            Assertions.assertFalse(tree.containsVertex(v));
+        }
+
+        Assertions.assertEquals(tree.getHeight(), 2);
+
+        Assertions.assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdge() {
+        Assertions.assertTrue(tree.addVertex(v1));
+        Assertions.assertTrue(tree.addEdge(v1, v2));
+        Assertions.assertFalse(tree.addEdge(v3, v4));
+        Assertions.assertFalse(tree.addEdge(v1, v2));
+        Assertions.assertFalse(tree.addEdge(v3, v2));
+
+        Assertions.assertTrue(checkInv());
+    }
 }
