b982917eaf14056a70c188894b3632496f0fedd4
beta <mzg00@postech.ac.kr>
2021-10-01 18:51:43 +0900

    💯 Impl. AbstractMutableGraphTest

--- src/test/java/edu/postech/csed332/homework3/AbstractMutableGraphTest.java
+++ src/test/java/edu/postech/csed332/homework3/AbstractMutableGraphTest.java
@@ -54,7 +54,61 @@ public abstract class AbstractMutableGraphTest<V extends Comparable<V>, G extend
         Assertions.assertTrue(checkInv());
     }
 
-    // TODO: write black-box test cases for each method of MutableGraph with respect to
-    //  the specification, including the methods of Graph that MutableGraph extends.
+    @Test
+    void testRemoveEdge() {
+        graph.addEdge(v1, v2);
+        Assertions.assertTrue(graph.removeEdge(v1, v2));
+        Assertions.assertFalse(graph.removeEdge(v1, v2));
+        Assertions.assertTrue(checkInv());
+    }
 
+    @Test
+    void testRemoveVertex() {
+        graph.addEdge(v1, v2);
+        graph.addEdge(v2, v3);
+        graph.addEdge(v4, v2);
+
+        Assertions.assertTrue(graph.removeVertex(v2));
+        Assertions.assertFalse(graph.removeVertex(v2));
+
+        Assertions.assertEquals(graph.getTargets(v1), Set.of());
+        Assertions.assertEquals(graph.getTargets(v4), Set.of());
+        Assertions.assertEquals(graph.getSources(v3), Set.of());
+        Assertions.assertTrue(checkInv());
+    }
+
+    @Test
+    void testAddEdge() {
+        Assertions.assertTrue(graph.addEdge(v1, v2));
+        Assertions.assertFalse(graph.addEdge(v1, v2));
+
+        Assertions.assertEquals(graph.getTargets(v1), Set.of(v2));
+        Assertions.assertEquals(graph.getSources(v2), Set.of(v1));
+        Assertions.assertTrue(checkInv());
+    }
+
+    @Test
+    void testBidirectional() {
+        Assertions.assertTrue(graph.addEdge(v1, v2));
+        Assertions.assertTrue(graph.addEdge(v1, v4));
+        Assertions.assertTrue(graph.addEdge(v2, v3));
+        Assertions.assertTrue(graph.addEdge(v3, v2));
+        Assertions.assertTrue(graph.addEdge(v3, v4));
+
+        Assertions.assertEquals(graph.getTargets(v2), Set.of(v3));
+        Assertions.assertEquals(graph.getTargets(v3), Set.of(v2, v4));
+
+        Assertions.assertEquals(graph.getSources(v2), Set.of(v1, v3));
+        Assertions.assertEquals(graph.getSources(v3), Set.of(v2));
+
+        Assertions.assertTrue(graph.removeVertex(v3));
+        Assertions.assertEquals(graph.getVertices(), Set.of(v1, v2, v4));
+
+        Assertions.assertEquals(graph.getTargets(v2), Set.of());
+        Assertions.assertEquals(graph.getTargets(v3), Set.of());
+
+        Assertions.assertEquals(graph.getSources(v2), Set.of(v1));
+        Assertions.assertEquals(graph.getSources(v3), Set.of());
+        Assertions.assertEquals(graph.getSources(v4), Set.of(v1));
+    }
 }
