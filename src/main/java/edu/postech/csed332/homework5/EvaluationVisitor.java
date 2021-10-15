0c2941183a67e0fc8f56540c4e457018891febb6
beta <mzg00@postech.ac.kr>
2021-10-15 18:48:14 +0900

    💼 Impl. EvaluationVisitor

--- src/main/java/edu/postech/csed332/homework5/EvaluationVisitor.java
+++ src/main/java/edu/postech/csed332/homework5/EvaluationVisitor.java
@@ -1,6 +1,9 @@
 package edu.postech.csed332.homework5;
 
+import edu.postech.csed332.homework5.expression.*;
+
 import java.util.Map;
+import java.util.function.BinaryOperator;
 
 /**
  * A visitor to evaluate a value of an expression, given a valuation for each variable
@@ -12,6 +15,42 @@ public class EvaluationVisitor implements ExpVisitor<Double> {
         this.valuation = valuation;
     }
 
-    // TODO write and implement the visitor methods for EvaluationVisitor, satisfying
-    //  the specification of Exp.eval.
+    @Override
+    public Double visit(VariableExp exp) {
+        return valuation.get(exp.getName());
+    }
+
+    @Override
+    public Double visit(NumberExp exp) {
+        return exp.getValue();
+    }
+
+    @Override
+    public Double visit(PlusExp exp) {
+        return binaryExpEvaluator(exp, Double::sum);
+    }
+
+    @Override
+    public Double visit(MinusExp exp) {
+        return binaryExpEvaluator(exp, (x, y) -> x - y);
+    }
+
+    @Override
+    public Double visit(MultiplyExp exp) {
+        return binaryExpEvaluator(exp, (x, y) -> x * y);
+    }
+
+    @Override
+    public Double visit(DivideExp exp) {
+        return binaryExpEvaluator(exp, (x, y) -> x / y);
+    }
+
+    @Override
+    public Double visit(ExponentiationExp exp) {
+        return binaryExpEvaluator(exp, Math::pow);
+    }
+
+    private Double binaryExpEvaluator(BinaryExp exp, BinaryOperator<Double> f) {
+        return f.apply(exp.getLeft().accept(this), exp.getRight().accept(this));
+    }
 }
