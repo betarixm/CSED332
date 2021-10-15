383d62f3c5286cf78d505604577e29bb2eee0dae
beta <mzg00@postech.ac.kr>
2021-10-15 18:49:43 +0900

    🏄 ExpDecorator overrides basic methods

--- src/main/java/edu/postech/csed332/homework5/ExpDecorator.java
+++ src/main/java/edu/postech/csed332/homework5/ExpDecorator.java
@@ -1,6 +1,9 @@
 package edu.postech.csed332.homework5;
 
 import edu.postech.csed332.homework5.expression.Exp;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 /**
  * A base decorator class
@@ -12,6 +15,24 @@ public class ExpDecorator extends Exp {
         expression = e;
     }
 
-    // TODO implement all the methods of ExpDecorator
+    @Override
+    public String toString() {
+        return expression.toString();
+    }
 
+    @Override
+    @NotNull
+    public Double eval(@NotNull Map<Integer, Double> valuation) {
+        return expression.eval(valuation);
+    }
+
+    @Override
+    public boolean equiv(@NotNull Exp other) {
+        return expression.equiv(other);
+    }
+
+    @Override
+    public <T> @NotNull T accept(ExpVisitor<T> visitor) {
+        return expression.accept(visitor);
+    }
 }
