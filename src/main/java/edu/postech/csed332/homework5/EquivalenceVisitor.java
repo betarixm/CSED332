a9a864803dd86487bf4eb4248f1c33206bf5f583
beta <mzg00@postech.ac.kr>
2021-10-15 18:48:34 +0900

    💼 Impl. EquivalenceVisitor

--- src/main/java/edu/postech/csed332/homework5/EquivalenceVisitor.java
+++ src/main/java/edu/postech/csed332/homework5/EquivalenceVisitor.java
@@ -1,6 +1,6 @@
 package edu.postech.csed332.homework5;
 
-import edu.postech.csed332.homework5.expression.Exp;
+import edu.postech.csed332.homework5.expression.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -8,10 +8,49 @@ import org.jetbrains.annotations.NotNull;
  */
 public class EquivalenceVisitor implements ExpVisitor<Boolean> {
 
+    protected Exp other;
+
     public EquivalenceVisitor(@NotNull Exp other) {
-        // TODO: implement this
+        this.other = other;
     }
 
-    // TODO write and implement the visitor methods for EquivalenceVisitor, satisfying
-    //  the specification of Exp.equiv. You may need to add more member variables.
+    @Override
+    public Boolean visit(VariableExp exp) {
+        return other instanceof VariableExp && exp.getName() == ((VariableExp) other).getName();
+    }
+
+    @Override
+    public Boolean visit(NumberExp exp) {
+        return other instanceof NumberExp && exp.getValue().equals(((NumberExp) other).getValue());
+    }
+
+    @Override
+    public Boolean visit(PlusExp exp) {
+        return other instanceof PlusExp && binaryTermsSame(exp);
+    }
+
+    @Override
+    public Boolean visit(MinusExp exp) {
+        return other instanceof MinusExp && binaryTermsSame(exp);
+    }
+
+    @Override
+    public Boolean visit(MultiplyExp exp) {
+        return other instanceof MultiplyExp && binaryTermsSame(exp);
+    }
+
+    @Override
+    public Boolean visit(DivideExp exp) {
+        return other instanceof DivideExp && binaryTermsSame(exp);
+    }
+
+    @Override
+    public Boolean visit(ExponentiationExp exp) {
+        return other instanceof ExponentiationExp && binaryTermsSame(exp);
+    }
+
+    public Boolean binaryTermsSame(BinaryExp exp) {
+        BinaryExp binaryOther = (BinaryExp) other;
+        return exp.getLeft().accept(new EquivalenceVisitor(binaryOther.getLeft())) && exp.getRight().accept(new EquivalenceVisitor(binaryOther.getRight()));
+    }
 }
